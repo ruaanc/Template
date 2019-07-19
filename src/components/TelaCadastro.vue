9013faaf07392e9e3a725e546b1014e5e410b9a3
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-07-19 19:06:26 -0300

    Update de TelaCadastro.vue e criação de ListaUsuarios

--- src/components/TelaCadastro.vue
+++ src/components/TelaCadastro.vue
@@ -1,21 +1,41 @@
 <template>
+  <div id="temp">
     <div class="cadastro">
-        <form action="">
-            <fieldset>
-                <legend>Cadastro</legend>
-                <label>Nome: </label><input type="text" class="campo"><br>
-                <label>Email: </label><input type="email" class="campo"><br>
-                <label>Senha: </label><input type="password" class="campo"><br>
-                <input type="submit" class="btn-submit" value="Salvar">
-            </fieldset>
-        </form>
+      <form action>
+        <fieldset>
+          <legend>Cadastro</legend>
+          <label>Nome:</label>
+          <input type="text" class="campo" v-model="usuario.nome" />
+          <br />
+          <label>Email:</label>
+          <input type="email" class="campo" v-model="usuario.email" />
+          <br />
+          <label>Senha:</label>
+          <input type="password" class="campo" v-model="usuario.senha" />
+          <br />
+          <input type="submit" class="btn-submit" value="Salvar" />
+        </fieldset>
+      </form>
     </div>
+    <lista-usuarios/>
+  </div>
 </template>
 
 <script>
+import ListaUsuarios from './ListaUsuarios.vue'
 export default {
-
-}
+  components: {ListaUsuarios},  
+  data() {
+    return {
+        usuarios: [],
+        usuario: {
+            nome: "",
+            email: "",
+            senha:""
+        }
+    };
+  }
+};
 </script>
 
 <style scoped>
@@ -31,47 +51,43 @@ export default {
 }
 
 .cadastro fieldset {
-    border: double;
-    float: left;
-    padding: 5px 15px 0px;
+  border: double;
+  float: left;
+  padding: 5px 15px 0px;
 }
 
 .cadastro label {
-    margin-bottom: 0.2em;
-    color: #666;
-    display: block;
-    padding-top: 10px;
-    font-size: 2em;
+  margin-bottom: 0.2em;
+  color: #666;
+  display: block;
+  padding-top: 10px;
+  font-size: 2em;
 }
 
 .btn-submit {
-    margin-top: 10px;
-    font-size: 1.2em;
-    background: #F90;
-    border: 0;
-    margin-bottom: 1em;
-    color: #FFF;
-    padding: 0.2em 0.6em; 
-    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
-    cursor: pointer;
+  margin-top: 10px;
+  font-size: 1.2em;
+  background: #f90;
+  border: 0;
+  margin-bottom: 1em;
+  color: #fff;
+  padding: 0.2em 0.6em;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
 }
 
 .cadastro input {
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
-    padding: 5px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  padding: 5px;
 }
 
 .cadastro .campo {
-    padding-right: 400px;
+  padding-right: 400px;
 }
 
 .cadastro legend {
-    font-family: Arial, sans-serif;
-    font-size: 2em;
-    font-style: normal;
+  font-family: Arial, sans-serif;
+  font-size: 2em;
+  font-style: normal;
 }
-
-
-
-    
 </style>
