eab50a85f2b3af38daddd53c7b49c69bff8078cc
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-07-19 11:27:33 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <tela-cadastro/>
+  </div>
+</template>
+
+<script>
+import TelaCadastro from './components/TelaCadastro.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TelaCadastro
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TelaCadastro.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="cadastro">
+        <form action="">
+            <fieldset>
+                <legend>Cadastro</legend>
+                <label>Nome: </label><input type="text" class="campo"><br>
+                <label>Email: </label><input type="email" class="campo"><br>
+                <label>Senha: </label><input type="password" class="campo"><br>
+                <input type="button" class="btn-submit" value="Salvar">
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.cadastro {
+  position: relative;
+  margin: auto;
+  padding: 150px 0;
+  font-family: sans-serif;
+  font-size: 1em;
+  display: table;
+  clear: both;
+  margin-bottom: 1em;
+}
+
+.cadastro fieldset {
+    border: double;
+    float: left;
+    padding: 5px 10px 0px;
+}
+
+.cadastro label {
+    margin-bottom: 0.2em;
+    color: #666;
+    display: block;
+    padding-top: 10px;
+    font-size: 2em;
+}
+
+.btn-submit {
+    margin-top: 10px;
+    font-size: 1.2em;
+    background: #F90;
+    border: 0;
+    margin-bottom: 1em;
+    color: #FFF;
+    padding: 0.2em 0.6em; 
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+
+}
+
+.cadastro input {
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+    padding: 5px;
+}
+
+.cadastro .campo {
+    padding-right: 200px;
+}
+
+.cadastro legend {
+    font-family: Arial, sans-serif;
+    font-size: 2em;
+    font-style: normal;
+}
+
+
+    
+</style>
