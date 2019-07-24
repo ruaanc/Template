5ce65d87013c1677cc3556458b48b3d47c8a6d00
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-07-24 16:54:01 -0300

    Recebendo o Json

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
   <body>
     <div id="app">
       <tela-cadastro/>
+      {{usuarios}}
     </div>
   </body>
 </template>
 
 <script>
 import TelaCadastro from './components/TelaCadastro.vue'
+import User from './services/usr'
 
 export default {
   name: 'app',
   components: {
     TelaCadastro
-  }
+  },
+  data() {
+    return{
+      usuarios: null
+    } 
+  },
+  mounted() {
+      User.listar().then(response => {
+        this.usuarios = response
+      })
+  }    
 }
 </script>
 
