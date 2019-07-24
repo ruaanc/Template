495488a4cf796e11ffe9dbc0c34c18b15d3ac980
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-07-24 20:31:03 -0300

    Método post implementado !

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,52 @@
   <body>
     <div id="app">
       <tela-cadastro/>
-      {{usuarios}}
+      <lista-usuarios :usuarios="usuarios"/>
+      <form @submit.prevent="save">
+        <button>Enviar</button>
+      </form>
+      
     </div>
   </body>
 </template>
 
 <script>
 import TelaCadastro from './components/TelaCadastro.vue'
+import ListaUsuarios from './components/ListaUsuarios.vue'
 import User from './services/usr'
 
 export default {
   name: 'app',
   components: {
-    TelaCadastro
+    TelaCadastro,
+    ListaUsuarios,  
   },
   data() {
     return{
-      usuarios: null
+      usuarios: [],
+      usuario: {
+        username: 'Raíssa',
+        email: 'ruaissa.tec@gmail.com',
+        password: '12345',
+        password_confirmation: '12345'
+      }
     } 
   },
   mounted() {
       User.listar().then(response => {
-        this.usuarios = response
+        this.usuarios = response.data
       })
-  }    
+  },
+
+  methods: {
+    save(){
+      User.save(this.usuario).then(res => {
+        res.data = this.usuario
+        alert('Salvo co sucesso')
+      })
+    }
+  }
+
 }
 </script>
 
